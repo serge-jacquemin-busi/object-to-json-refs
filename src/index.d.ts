2a96a57e18b3f38902e9b4bc39291761fdeedda2
Serge Jacquemin <Serge.Jacquemin@BuSI.eu>
2017-05-22 15:18:26 +0200

    support typescript export

---
+++ src/index.d.ts
@@ -0,0 +1,5 @@
+// Type definitions for object-to-json-refs
+// Project: object-to-json-refs
+// Definitions by: Jacquemin Serge <http://busi.eu/>
+
+declare function MyFunction(name: string): void;
